b2b5fc7694040eb425c4cc216da4b4506872d921
cuonglhv-code <cuonglhv@gmail.com>
2026-03-26 17:50:22 +0000

    chore: download stitch assets for design reference

---
+++ download_screens.ps1
@@ -0,0 +1,18 @@
+$screens = @(
+    @("login", "https://lh3.googleusercontent.com/aida/ADBb0ugkKC3oYox-ILE_10YgQ02tfBkPgJjXyTO25dXkRT-JJEtFEGOpVWoVIWFfZjdGh6YwsUbvCqEXN9AbnzObmjaJD3NM6twbd71u6tgS4kG7WJrlCyr1itcKO4E3sxJVaVllc-88hkwXXsat3hX9YHqiyD_wQlvQrT63wRQYdu5mE5qoIz4fyQsfQwTUNQ7tSBsVyUyUIlueSO3VN0_zYYb6A0Aa9qe_Un9pNtLW8_v8kHybWPYme9Thw5k", "https://contribution.usercontent.google.com/download?c=CgthaWRhX2NvZGVmeBJ7Eh1hcHBfY29tcGFuaW9uX2dlbmVyYXRlZF9maWxlcxpaCiVodG1sXzQxMmIxNDJjMzY4YTQwOTFiMmJjMjU0M2YxYWVkY2IwEgsSBxD8lKrtshcYAZIBIwoKcHJvamVjdF9pZBIVQhMzOTc4MzEzMzIyNjg5NTcyNzE5&filename=&opi=89354086"),
+    @("landing", "https://lh3.googleusercontent.com/aida/ADBb0ujwyEbF_t0rDbo8t8FsL7rhUGYinCq6DJHgkw-BHCH1EWTqL_H0dB9IaanHvp0HsrOolCTK-uyvHcgf2umtSmZL_KSl5UHR-9mbVi29zmaqZ59-w4hA1MZDFtJbjQP7egJFtUscdUPkic2aqhg2IyGem7JLGTepxCjOi1xFF_dGY9hnvSvxG9UtVwAjuErpiDxJ1La3f8swcoUcM-oDelj-VLVShGmfRyEEtlVMuPdMnZdl_lZ7g49a-Q", "https://contribution.usercontent.google.com/download?c=CgthaWRhX2NvZGVmeBJ7Eh1hcHBfY29tcGFuaW9uX2dlbmVyYXRlZF9maWxlcxpaCiVodG1sXzY5ZTU3N2EzYjNhNTQzYTRiN2ZiNzc2YmZhYjRiNzMwEgsSBxD8lKrtshcYAZIBIwoKcHJvamVjdF9pZBIVQhMzOTc4MzEzMzIyNjg5NTcyNzE5&filename=&opi=89354086"),
+    @("writing101", "https://lh3.googleusercontent.com/aida/ADBb0uimexKl0NlYklI1tuB9dd2w6lPVMr3WJe2ZTMnfUjodxLtmmK3LPMD3xJBp5G1m0LfgzC8KkpBP0Qkm0kO1C7ZuLsOdMvr9_bxiA4ZcsuZjidgNsqrq2mjsHjOYmsms7oKO5lZstt2ZQrIaebTKKU-apIPY1soPfGvJEA1dMz1adTJigF_YAAyOSwnJ02q2HKGU4C8Jt6FzE_X7pAgUi4VwSwg2g-Rv5cjBX3p12nlVUNSfRr_FucuCKTs", "https://contribution.usercontent.google.com/download?c=CgthaWRhX2NvZGVmeBJ7Eh1hcHBfY29tcGFuaW9uX2dlbmVyYXRlZF9maWxlcxpaCiVodG1sXzc1MTExNTZiNWUxMzQwNGVhNWM4Zjc2MGRkNzQ3YmVjEgsSBxD8lKrtshcYAZIBIwoKcHJvamVjdF9pZBIVQhMzOTc4MzEzMzIyNjg5NTcyNzE5&filename=&opi=89354086"),
+    @("practice_writing", "https://lh3.googleusercontent.com/aida/ADBb0ujFf66UaVAgybnKfyppX4pyKTTliseNb-DpWZg0tjtWxJkdz7-RKR8cQ4ety5Z89BluT0zHv-hy5rsF8pJf5lwEIPFI70y38iScf9PwfRfz6pzIJRrySCDej7Q3IyqAP0qmgwiVmVkxlQFjv3Q005ANHGVKMJmfTtI7w4uaQaQQm2tfdvXl4URPNM4DK4D6XmOJ-YYHg2dFga067XmOkVdCCPJyxnrxbWQm1ASRzN0N0q979hQZW0fIOuY", "https://contribution.usercontent.google.com/download?c=CgthaWRhX2NvZGVmeBJ7Eh1hcHBfY29tcGFuaW9uX2dlbmVyYXRlZF9maWxlcxpaCiVodG1sXzU3MGEwZWNmY2ExMzQ0ZjJhNGZlMjZkZmRmMWI5MTJhEgsSBxD8lKrtshcYAZIBIwoKcHJvamVjdF9pZBIVQhMzOTc4MzEzMzIyNjg5NTcyNzE5&filename=&opi=89354086"),
+    @("landing_mobile", "https://lh3.googleusercontent.com/aida/ADBb0uhf9Ecnj19hXCEPsAv7PpH0a-YN9bRNPokJThl1kTPLL0aQ-paIAMBSLKzDIjA6l5xv7bxDdiw40ELgN7tEt5uKlNQPCBESkd3-J1Ihvq3AplJ-AycpcmX0Q6c_N8BIdZZL1JrugFqvpWxwPUbsVkfFdnTc6GjBu9pDIxdpm8CRkCtaANWIoSjRPkSaNYFxLv8SDEqklwH7uUW51skEfCsMQ5ggslVxSZ65Ul1TPj7a9iIJ6yTHcaY6diE", "https://contribution.usercontent.google.com/download?c=CgthaWRhX2NvZGVmeBJ7Eh1hcHBfY29tcGFuaW9uX2dlbmVyYXRlZF9maWxlcxpaCiVodG1sXzYyY2YxNDU2MDEyZDQyZTdhNGM4MGRkMzAwYWUzZTBlEgsSBxD8lKrtshcYAZIBIwoKcHJvamVjdF9pZBIVQhMzOTc4MzEzMzIyNjg5NTcyNzE5&filename=&opi=89354086")
+)
+
+# Use curl.exe directly to bypass Invoke-WebRequest issues
+foreach ($screen in $screens) {
+    $name = $screen[0]
+    $imgUrl = $screen[1]
+    $htmlUrl = $screen[2]
+    
+    Write-Host "Downloading $name..."
+    & curl.exe -L "$imgUrl" -o "stitch-assets/$name.png"
+    & curl.exe -L "$htmlUrl" -o "stitch-assets/$name.html"
+}
